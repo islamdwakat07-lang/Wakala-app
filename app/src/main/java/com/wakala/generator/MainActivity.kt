9e274884fb5a8441a59ee7c943671daf04bde0a0
islamdwakat07-lang <islamdwakat07@gmail.com>
2026-09-18 12:11:34 +0300

    Update MainActivity.kt

--- app/src/main/java/com/wakala/generator/MainActivity.kt
+++ app/src/main/java/com/wakala/generator/MainActivity.kt
@@ -32,7 +32,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CompositionLocalProvider
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
